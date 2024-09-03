9af57dff497f78a7cd63631004ccc9654075b6c3
bossscrabb <1625116337@qq.com>
2024-09-03 23:34:59 +0800

    9-3

---
+++ cmd/go_code/goroutine/demo1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 主协程
+func main() {
+
+	// 主死从随
+
+	// 1.闭包 2.for循环问题，for循环的时候 每个面两会重用
+	// for循环的时候，i变量会被重用
+
+	//匿名函数启动goroutine (每次打印的值不一样)
+	for i := 0; i < 100; i++ {
+		// 第一种解决办法 tem := i
+		go func(i int) { // 第二种方法，调用i，使用值传递，更优雅
+			fmt.Println(i)
+		}(i) // 值传递
+	}
+
+	fmt.Println("main goroutine")
+	time.Sleep(10 * time.Second)
+}
---
+++ cmd/go_code/goroutine/waitGroup.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 	time.Sleep(10 * time.Second)
+// 不写time.Sleep
+// 子的goroutine如何通知主goroutine自己结束了，主的goroutine如何知道子goroutine已经结束了
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	// 这里有三个方法
+	// 这里表示要监控多少个goroutine执行结束 ，这里是100个
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+
+		go func(i int) {
+			//一般先写这一句
+			defer wg.Done() // 因为是defer，这一句肯定会在匿名函数执行完成之后才执行
+			fmt.Println(i)
+
+		}(i)
+	}
+
+	// 等到
+	wg.Wait()
+	fmt.Println("all done")
+
+	// waitgroup主要用于goroutine的执行等待，Add方法要和Done成对出现
+}
